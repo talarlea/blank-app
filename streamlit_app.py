9c623c59c15bd755caab9e93a30268b202270ead
Leah Talary <talarlea@students.zhaw.ch>
2026-03-25 13:16:34 +0100

    unmute

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("Mahna Mahna")
 st.write("You have been Bamboozled!")
 
-# Auto-play the YouTube video (muted to satisfy autoplay policies in browsers)
+# Auto-play the YouTube video
 video_id = "QTXyXuqfBLA"
-embed_url = f"https://www.youtube.com/embed/{video_id}?autoplay=1&mute=1&controls=1"
+embed_url = f"https://www.youtube.com/embed/{video_id}?autoplay=1&mute=0&controls=1"
 
 st.markdown(
     f"<iframe width=\"100%\" height=\"480\" src=\"{embed_url}\" frameborder=\"0\" allow=\"autoplay; encrypted-media; picture-in-picture\" allowfullscreen></iframe>",
