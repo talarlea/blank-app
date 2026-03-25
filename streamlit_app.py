6f0aff14cdcd6428043d6aded51b75752fe4d3b0
Leah Talary <talarlea@students.zhaw.ch>
2026-03-25 13:04:07 +0100

    mahna mahna

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Mahna Mahna")
+st.write("You have been Bamboozled!")
+
+# Auto-play the YouTube video (muted to satisfy autoplay policies in browsers)
+video_id = "QTXyXuqfBLA"
+embed_url = f"https://www.youtube.com/embed/{video_id}?autoplay=1&mute=1&controls=1"
+
+st.markdown(
+    f"<iframe width=\"100%\" height=\"480\" src=\"{embed_url}\" frameborder=\"0\" allow=\"autoplay; encrypted-media; picture-in-picture\" allowfullscreen></iframe>",
+    unsafe_allow_html=True,
 )
+
